bab91af25cc0bbf9486293c1c2528857c244d71e
Mathias Roux <mathiasroux.pro@gmail.com>
2021-11-20 11:08:43 +0100

    stupid coach rails app

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,16 +1,31 @@
 class QuestionsController < ApplicationController
+  QUESTION_WORK = 'I am going to work'.freeze
+  ANSWER_WORK = 'Great!'.freeze
+  ANSWER_QUESTION = 'Silly question, get dressed and go to work!'.freeze
+  ANSWER_ELSE = "I don't care, get dressed and go to work!".freeze
+
   def ask
     # qfdbz
   end
 
   def answer
     @question = params[:question]
-    if @question == 'I am going to work'
-      @answer = 'Great!'
-    elsif @question.include? '?'
-      @answer = 'Silly question, get dressed and go to work!'
+    stupid_coach(@question)
+  end
+
+  private
+
+  def stupid_coach(question)
+    if question?(question)
+      @answer = ANSWER_QUESTION
+    elsif question == QUESTION_WORK
+      @answer = ANSWER_WORK
     else
-      @answer = "I don't care, get dressed and go to work!"
+      @answer = ANSWER_ELSE
     end
+  end
+
+  def question?(string)
+    string.nil? ? false : string.end_with?('?')
   end
 end
